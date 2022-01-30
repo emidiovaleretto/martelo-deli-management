ecaac66364be168623a68d2a5567fd76f39444bd
Emidio Valereto <emidio.valereto@gmail.com>
2022-01-30 16:47:06 +0000

    Add remove_category method to ControllerCategory.

--- controller.py
+++ controller.py
@@ -1,8 +1,12 @@
-from DAO import  DaoCategory
+from DAO import  DaoCategory, DaoSales
+from models import Product, Category, Client, Employee, Sales, Stock
 
 class ControllerCategory:
 
 	def register_new_category(self, category):
+
+		''' This method will register a new category in the system 
+			and will save it in the categories.txt file. '''
 
 		exists = False
 
@@ -17,5 +21,27 @@ class ControllerCategory:
 			print("Category saved successfully!")
 		else:
 			print("Category already exists!")
+
+	def remove_category(self, category_to_remove):
+
+		''' This method will remove a category from the system 
+			and will remove it from the categories.txt file. '''
+
+		data = DaoCategory.read()
+		categories = filter(lambda x: x.category == category_to_remove, data)
+
+		if len(categories) == 0:
+			print("Category not found!")
+		else:	
+			for i in range(len(categories)):
+				if categories[i] == category_to_remove:
+					del categories[i]
+					break
+				print("Removed category successfully!")
+
+			with open("categories.txt", "w") as f:
+				for i in data:
+					f.writelines(f"{i.category}\n")
+
 
 
