7865c5bef53dac4e9be69067942227711f9e47ae
Emidio Valereto <emidio.valereto@gmail.com>
2022-01-31 17:46:29 +0000

    Organize views.py.

--- views.py
+++ views.py
@@ -1,18 +1,4 @@
-from controller import ControllerCategory
-from DAO import DaoCategory
-from random import randint
-from category import category
-
-def generate_number(param):
-	''' Generates a random number between 
-		1.000 and 5.000 for Client instances and
-		10.000 and 50.000 for Employee instances. '''
-
-	if param == "client":
-		return randint(1_000, 5_000)
-	elif param == "employee":
-		return randint(10_000, 50_000)
-
+from utilities.category import category
 
 while True:
 	print('''
