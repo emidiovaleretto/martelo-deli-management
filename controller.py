475c12e2ca169ee40f24204138bde58c9207bbe8
Emidio Valereto <emidio.valereto@gmail.com>
2022-01-31 16:41:40 +0000

    Add read_categories and update_category functions to controller.

--- controller.py
+++ controller.py
@@ -12,8 +12,8 @@ class ControllerCategory:
 
 		response = DaoCategory.read()
 
-		for category_name in response:
-			if category_name == category:
+		for cat in response:
+			if cat.category_name == category:
 				exists = True
 
 		if not exists:
@@ -22,7 +22,39 @@ class ControllerCategory:
 		else:
 			print("Category already exists!")
 
+	def read_categories():
 
+		''' This method will list all the categories from 
+			categories.txt file. '''
+
+		response = DaoCategory.read()
+
+		if len(response) <= 0:
+			print("There are no categories in the system!")
+		else:
+			for category in response:
+				print(f"Category: {category.category_name}")
+
+	def update_category(category_to_update, new_category):
+		''' This function updates an existing category based from
+		    the categories.txt file '''
+
+		response = DaoCategory.read()
+		categories = list(filter(lambda x: x.category_name == category_to_update, response))
+
+		if len(categories) <= 0:
+			print("Category not found!")
+		else:
+			for i in range(len(response)):
+				if response[i].category_name == category_to_update:
+					response[i].category_name = new_category
+					break
+			print("Category updated successfully!")
+
+			with open("categories.txt", "w") as f:
+				for i in response:
+					f.writelines(f"{i.category_name}\n")
+	
 	def remove_category(category_to_remove):
 
 		''' This method will remove a category from the system 
